f961cd00eef7319c1886465aa029f70f1328203f
catastroface <barna.urmossy@gmail.com>
2021-10-11 09:13:56 +0200

    Fix minor styling issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,12 +30,17 @@ const skillsContent = document.getElementsByClassName("skills__content"),
 function toggleSkills() {
   let itemClass = this.parentNode.className;
 
+  //Autohide all other lists. Remove to keep them opened.
   for (i = 0; i < skillsContent.length; i++) {
     skillsContent[i].className = "skills__content skills__close";
   }
   if (itemClass === "skills__content skills__close") {
     this.parentNode.className = "skills__content skills__open";
   }
+  // Manually close lists.
+  // else if (itemClass === "skills__content skills__open") {
+  //   this.parentNode.className = "skills__content skills__close";
+  // }
 }
 
 skillsHeader.forEach((el) => {
