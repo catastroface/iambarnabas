72b5e6582862a380c66c0120f6635dba18cdd5a1
catastroface <barna.urmossy@gmail.com>
2022-01-07 22:16:00 +0100

    Implement logic for scroll to top button to show on scroll position

--- assets/js/main.js
+++ assets/js/main.js
@@ -152,3 +152,11 @@ function scrollHeader() {
   else nav.classList.remove("scroll-header");
 }
 window.addEventListener("scroll", scrollHeader);
+
+// Show scroll top
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
